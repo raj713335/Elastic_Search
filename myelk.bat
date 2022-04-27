a04a84bdc2ac00f0706c2c4f31d99e2c558f5252
ARNAB DAS <raj713335@gmail.com>
2022-04-27 23:46:23 +0000

    Add files via upload

---
+++ myelk.bat
@@ -0,0 +1,4 @@
+echo Starting ELK Stack
+START cmd /C C:\Users\ZEUS\Desktop\Software\elasticsearch-8.1.3\bin\elasticsearch.bat
+echo starting Kibana
+START cmd /C C:\Users\ZEUS\Desktop\Software\kibana-8.1.3\kibana.bat
